8c0f8d15e38a75a8df65b30031a823657d4640a2
Pdtcode <trinhpeter15@gmail.com>
2024-08-04 18:20:44 -0500

    revert webm change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,13 +4,12 @@ import Image from 'next/image'
 function Hero() {
   return (
     <div className="relative flex flex-col items-center justify-center h-full w-full">
-        <video 
-          autoPlay 
-          muted 
-          loop
-          playsInline
-          className="absolute inset-0 h-full w-full object-cover z-0"
-          src="/tabgvid.webm"
+         <Image
+          src='/tabgvid.gif'
+          layout='fill'
+          objectFit='cover'
+          alt='bg gif'
+          unoptimized
         />
         <div className="absolute inset-0 bg-amber-900/35 z-10"></div>
         <div className="relative mb-64 z-20">
